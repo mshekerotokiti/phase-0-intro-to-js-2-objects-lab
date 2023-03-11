874f437a6e3084fdb8f1db20d32ce57f8a66c641
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-11 12:32:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee ={
+    name: "Sharon",
+    streetAddress:"Nairobi",
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newObj = { ...object };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+  }
+  
+  function deleteFromEmployeeByKey(object, key) {
+    const newObject = Object.assign({}, object);
+    delete newObject[key];
+    return newObject;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(object, key) {
+  delete object[key];
+  return object;
+  }
